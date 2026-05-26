2e9babe1e8e88a1463b2888848a6935dce700856
thanhdatd669-ops <thanhdatd669@gmail.com>
2026-05-26 19:24:02 +0700

    Create Main.java

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = null; 
+        int length = text.length();
+        System.out.println(length);
+    }
+}
